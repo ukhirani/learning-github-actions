9a55fce164e9fc6e64e17ad67f940facf5a49db1
Umang Hirani <umanghirani.exe@gmail.com>
2026-01-26 19:41:40 +0530

    commit

---
+++ project2/app/app.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "Hello from DevOps Pipeline"
+echo "Build time: $(date)"
---
+++ project2/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+if [ ! -f app/app.sh ]; then
+  exit 1
+fi
+
+echo "Build stage passed !!"
+
